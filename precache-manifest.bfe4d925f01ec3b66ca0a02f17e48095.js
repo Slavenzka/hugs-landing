82820c79d1688eb0a468083dd5bfc456e26300f0
Ivan <i.romanov@protonmail.com>
2020-09-22 18:17:23 +0300

    Updates

---
+++ precache-manifest.bfe4d925f01ec3b66ca0a02f17e48095.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064ef558b97f0ecf2d5f32f2aaa70127",
+    "url": "/hugs-landing/index.html"
+  },
+  {
+    "revision": "65227b80b5c7f75b3491",
+    "url": "/hugs-landing/static/css/2.456c216f.chunk.css"
+  },
+  {
+    "revision": "3a493b9a6375ec57b1f0",
+    "url": "/hugs-landing/static/css/main.30a866ea.chunk.css"
+  },
+  {
+    "revision": "65227b80b5c7f75b3491",
+    "url": "/hugs-landing/static/js/2.0d64ad90.chunk.js"
+  },
+  {
+    "revision": "5b099948b207b77f6fa04ff33fa98702",
+    "url": "/hugs-landing/static/js/2.0d64ad90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a493b9a6375ec57b1f0",
+    "url": "/hugs-landing/static/js/main.d73d67de.chunk.js"
+  },
+  {
+    "revision": "b1b27f6e16ece4236635",
+    "url": "/hugs-landing/static/js/runtime-main.97655a96.js"
+  },
+  {
+    "revision": "231495e51444c162bed5df57ee968c8e",
+    "url": "/hugs-landing/static/media/FuturaPT-Book.231495e5.woff2"
+  },
+  {
+    "revision": "54a123e5410e5dadf16d8b368fa0ab54",
+    "url": "/hugs-landing/static/media/FuturaPT-Book.54a123e5.woff"
+  },
+  {
+    "revision": "081741b541afdaa972cb5f76ac6d3a5e",
+    "url": "/hugs-landing/static/media/FuturaPT-ExtraBold.081741b5.woff2"
+  },
+  {
+    "revision": "11ba3ac6bb29be79323bda60fb72e9fb",
+    "url": "/hugs-landing/static/media/FuturaPT-ExtraBold.11ba3ac6.woff"
+  },
+  {
+    "revision": "7813a81a9a31f54d193bbfdc4cec0e63",
+    "url": "/hugs-landing/static/media/FuturaPT-Light.7813a81a.woff2"
+  },
+  {
+    "revision": "c89fa27abbcba4de1d9606fdfed9dcce",
+    "url": "/hugs-landing/static/media/FuturaPT-Light.c89fa27a.woff"
+  },
+  {
+    "revision": "8fa18513e8b92c123707ed12dc54b9da",
+    "url": "/hugs-landing/static/media/FuturaPT-Medium.8fa18513.woff"
+  },
+  {
+    "revision": "9819eaf042d4ad45186a5b49dfae95b4",
+    "url": "/hugs-landing/static/media/FuturaPT-Medium.9819eaf0.woff2"
+  },
+  {
+    "revision": "04b82b15da4bf7682d0944a396365143",
+    "url": "/hugs-landing/static/media/FuturaPTBold.04b82b15.woff2"
+  },
+  {
+    "revision": "b1755e863dafc58f2dd34f8ba8f7479b",
+    "url": "/hugs-landing/static/media/FuturaPTBold.b1755e86.woff"
+  },
+  {
+    "revision": "1726f096cc123b7eaa1351f200227471",
+    "url": "/hugs-landing/static/media/FuturaPTDemi.1726f096.woff"
+  },
+  {
+    "revision": "29fa49c1fce0e2d03503cfee4a50e9f9",
+    "url": "/hugs-landing/static/media/FuturaPTDemi.29fa49c1.woff2"
+  },
+  {
+    "revision": "7cbc67aed4e302fa9db536de984aacc1",
+    "url": "/hugs-landing/static/media/HUG’S.7cbc67ae.svg"
+  },
+  {
+    "revision": "0875b0be0fb09a60f27756937aaa497c",
+    "url": "/hugs-landing/static/media/bg_eric.0875b0be.png"
+  },
+  {
+    "revision": "b5f881d700d8b8aa2981214ab7d2f5ee",
+    "url": "/hugs-landing/static/media/bg_sherbina.b5f881d7.png"
+  },
+  {
+    "revision": "78b76f308807184bc9ba0d8c42ae492f",
+    "url": "/hugs-landing/static/media/bg_shvets.78b76f30.png"
+  },
+  {
+    "revision": "7ed974271570d34a133c80baffe40521",
+    "url": "/hugs-landing/static/media/card__house.7ed97427.svg"
+  },
+  {
+    "revision": "265f83c75183ec7ece3e49dbfe53e443",
+    "url": "/hugs-landing/static/media/card__palm.265f83c7.svg"
+  },
+  {
+    "revision": "a0c4afc13e33959eb1263a56ec5149dc",
+    "url": "/hugs-landing/static/media/card__pension.a0c4afc1.svg"
+  },
+  {
+    "revision": "f2e6ba79da00b92b29f8c5816e1f3ed0",
+    "url": "/hugs-landing/static/media/card__study.f2e6ba79.svg"
+  },
+  {
+    "revision": "28c36ea3f3c67237a77a0d7560ff19b0",
+    "url": "/hugs-landing/static/media/chart.28c36ea3.png"
+  },
+  {
+    "revision": "adb79efd0d6ad512597e9c566ca4f63b",
+    "url": "/hugs-landing/static/media/ic_check.adb79efd.svg"
+  },
+  {
+    "revision": "7fe07a1ee8fb6ba30253e48b37946af1",
+    "url": "/hugs-landing/static/media/ic_hugs.7fe07a1e.svg"
+  },
+  {
+    "revision": "00c16db60ce427a43a076608fd9a54a2",
+    "url": "/hugs-landing/static/media/money_02.00c16db6.png"
+  },
+  {
+    "revision": "769f8c480ab4db5a2bc8a6eba928cbae",
+    "url": "/hugs-landing/static/media/phone.769f8c48.png"
+  },
+  {
+    "revision": "4c4263f2b081c844b36d2469e27e56de",
+    "url": "/hugs-landing/static/media/phone@2x.4c4263f2.png"
+  },
+  {
+    "revision": "3721bce84c837c4c2fc2bd6ec18ca13c",
+    "url": "/hugs-landing/static/media/phone_02.3721bce8.png"
+  }
+]);
